cdd5e040fafcf17a263b2c6c0fc737f34eb76798
AdrianArroyoC <adrianarroyoceja@gmail.com>
2020-04-22 00:39:27 -0500

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -16,3 +16,9 @@ buttonPlay.onclick = () => player.togglePlay();
 
 const buttonMute = document.querySelector('#mute');
 buttonMute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
