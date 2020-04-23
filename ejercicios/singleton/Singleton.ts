8085cd5a173e7ca15cc070d4460df82f54d6892c
AdrianArroyoC <adrianarroyoceja@gmail.com>
2020-04-22 23:46:11 -0500

    Implementación del patrón Singleton

---
+++ ejercicios/singleton/Singleton.ts
@@ -0,0 +1,17 @@
+class Singleton {
+    private static instance: Singleton;
+
+    private constructor() {
+        // init
+    }
+
+    static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton
+        }
+        return Singleton.instance;
+    }
+
+}
+
+export default Singleton;
